c75a46bd1f68760766f152f2bccd5a70bf685ba4
ammkutti2310 <naiduiitkgp21@gmail.com>
2022-03-26 22:58:59 +0000

    Database initializationand API is added

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const dbPath = path.join(__dirname, "cricketMatchDetails.db");
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () =>
+      console.log("server running at http://localhost:3000/")
+    );
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+  }
+};
+
+initializeDBAndServer();
+
+app.get("/players/", async (request, response) => {
+  const getAllPlayersQuery = `SELECT * FROM player_details;`;
+  const allPlayersArray = await db.all(getAllPlayersQuery);
+  response.send(
+    allPlayersArray.map((eachPlayer) => {
+      return {
+        playerId: eachPlayer.player_id,
+        playerName: eachPlayer.player_name,
+      };
+    })
+  );
+});
